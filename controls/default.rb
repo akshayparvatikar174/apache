cf5e795f4ee4ed4eb9af2bcac3722c8c106240a2
akshayparvatikar174 <apravati@progress.com>
2025-01-29 14:10:02 +0530

    Delete controls directory

--- controls/default.rb
+++
@@ -1,8 +0,0 @@
-control 'apache-version' do
-  impact 1.0
-  title 'version check'
-  desc 'Check if apache version is installed'
-    describe command ('apachectl -v') do
-    its('stdout') {should match /2\.4\.58/}
-  end
-end
