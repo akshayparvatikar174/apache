2a28fbf9fd8ab69614a37deaf1a082ab26909dda
akshayparvatikar174 <apravati@progress.com>
2025-01-29 13:54:38 +0530

    Update default.rb

--- controls/default.rb
+++ controls/default.rb
@@ -3,6 +3,6 @@ control 'apache-version' do
   title 'version check'
   desc 'Check if apache version is installed'
     describe command ('apachectl -v') do
-    its('stdout') { should match (/Apache\/2\.4\.58/)}
+    its('stdout') {should match /2\.4\.58/}
   end
 end
