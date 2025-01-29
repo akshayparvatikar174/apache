ade66aa137726469e11eec9a5ce2fa80cfa960d7
akshayparvatikar174 <apravati@progress.com>
2025-01-29 12:04:02 +0530

    Update default.rb

--- controls/default.rb
+++ controls/default.rb
@@ -1,8 +1,15 @@
-control 'apache-version' do
+control 'apache-01' do
   impact 1.0
-  title 'version check'
-  desc 'Check if apache version is installed'
-  describe apache2 do
-    its('version') { should eq '2.4.58' }
+  title 'Check Apache2 Version'
+  desc 'Ensure that Apache2 is installed and running the correct version'
+
+  # Check if Apache2 is installed
+  describe package('apache2') do
+    it { should be_installed }
+  end
+
+  # Get Apache2 version and check if it's within a valid range
+  describe command('apache2 -v') do
+    its('stdout') { should match /Apache\/2\.\d+\.\d+/ }
   end
 end
