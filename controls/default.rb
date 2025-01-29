fec81000abd6b28417bdb89e83c26debbd13ed1c
akshayparvatikar174 <apravati@progress.com>
2025-01-29 14:49:08 +0530

    Update default.rb

--- controls/default.rb
+++ controls/default.rb
@@ -1,7 +1,7 @@
-control 'nginx-version' do
+control 'apache-version' do
   impact 1.0
   title 'version check'
-  desc 'Check if nginx version is installed'
+  desc 'Check for apache version'
   describe command('apachectl -v') do
     its('stdout') { should match /1\.4\.58/ }
   end
