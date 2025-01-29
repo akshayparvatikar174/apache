af7ccd05f33858c96d3f037db3c43931d3e1dfc9
akshayparvatikar174 <apravati@progress.com>
2025-01-29 11:18:11 +0530

    Update default.rb

--- controls/default.rb
+++ controls/default.rb
@@ -1,8 +1,8 @@
-control 'nginx-version' do
+control 'apache-version' do
   impact 1.0
   title 'version check'
-  desc 'Check if nginx version is installed'
-  describe nginx do
-    its('version') { should eq '1.24.0' }
+  desc 'Check if apache version is installed'
+  describe apache2 do
+    its('version') { should eq '2.4.58' }
   end
 end
